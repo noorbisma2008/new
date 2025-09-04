7df04f34a0c812a830a8a7ef09340319c720511a
noorbisma2008 <noorsadia238@gmail.com>
2025-09-03 22:22:08 -0700

    Add files via upload
    xyz

---
+++ script2.js
@@ -0,0 +1,8 @@
+var typed = new(".multiple-text", {
+    strings: ["Frontend Developer", "Graphic Designer", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+
+})
